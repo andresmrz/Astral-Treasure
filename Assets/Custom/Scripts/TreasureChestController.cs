e0b26059726d1498d78e8d2cfa697de66070aeea
Nicolas Murillo <uninoma_20@hotmail.com>
2022-02-23 23:31:37 -0500

    Organizando archivos

---
+++ Assets/Custom/Scripts/TreasureChestController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesoroController : MonoBehaviour
+{
+    private Animator animator;
+    public AudioSource source;
+    public AudioClip clip_open;
+    public AudioClip clip_close;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if(animator.GetInteger("AnimIndex") != 1)
+            {
+                source.clip = clip_open;
+                source.Play();
+
+                animator.SetInteger("AnimIndex", 1);
+                animator.SetTrigger("Next");
+			}
+        }
+    }
+
+    private void OnTriggerExit(Collider colicion)
+    {
+        if(colicion.tag == "Player")
+        {
+            if(animator.GetInteger("AnimIndex") == 1)
+            {
+                source.clip = clip_close;
+                source.Play();
+
+                animator.SetInteger("AnimIndex", 2);
+                animator.SetTrigger("Next");
+            }
+        }
+    }
+}
